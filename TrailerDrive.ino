7c4ea4d9b937286bc6c939388594d51e83238f33
Sam Faull <samz5991@hotmail.co.uk>
2017-09-17 11:14:45 +0100

    Changed pin assignments so that they physically align with the relay pin headers

--- TrailerDrive.ino
+++ TrailerDrive.ino
@@ -1,19 +1,18 @@
 #define INTERVAL 3000 // Time between switching outputs (in seconds)
 
-#define PB0  2 // Mode change switch at pin 2
-#define PB1  3 // Manual select switch at pin 3
+#define PB0  2  // Mode change switch at pin 2
+#define PB1  3  // Manual select switch at pin 3
+#define LED0 4  // LED 0 at pin 4
+#define LED1 5  // LED 1 at pin 5
 
-#define RELAY_0 6   // Relay channel 0 at pin 6
-#define RELAY_1 7   // Relay channel 1 at pin 7
-#define RELAY_2 8   // Relay channel 2 at pin 8
-#define RELAY_3 9   // Relay channel 3 at pin 9
-#define RELAY_4 10  // Relay channel 4 at pin 10
-#define RELAY_5 11  // Relay channel 5 at pin 11
-#define RELAY_6 12  // Relay channel 6 at pin 12
-
-#define LED0 A0  // LED 0 at pin A0
-#define LED1 A1  // LED 1 at pin A1
-#define LED2 A2  // LED 2 at pin A2
+#define RELAY_0 A0    // Relay channel 0 at pin A1
+#define RELAY_1 A1    // Relay channel 1 at pin A2
+#define RELAY_2 A2    // Relay channel 2 at pin A3
+#define RELAY_3 A3    // Relay channel 3 at pin A4
+#define RELAY_4 A4    // Relay channel 4 at pin A5
+#define RELAY_5 A5    // Relay channel 5 at pin A6
+#define RELAY_6 A6    // Relay channel 6 at pin A7
+#define RELAY_7 A7    // Relay channel 7 at pin A8 (CURRENTLY UNUSED)
 
 unsigned long currentMillis,      // runtime in ms
               previousMillis = 0; // used to store time checkpoints
@@ -32,6 +31,8 @@ void setup()
   pinMode(PB0, INPUT_PULLUP);  // Enables the internal pull-up resistor
   pinMode(PB1, INPUT_PULLUP);  // Enables the internal pull-up resistor
   //Outputs
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
   pinMode(RELAY_0, OUTPUT);
   pinMode(RELAY_1, OUTPUT);
   pinMode(RELAY_2, OUTPUT);
@@ -39,6 +40,7 @@ void setup()
   pinMode(RELAY_4, OUTPUT);
   pinMode(RELAY_5, OUTPUT);
   pinMode(RELAY_6, OUTPUT);
+  pinMode(RELAY_7, OUTPUT);
 
   // Initially set mode to display all outputs;
   STATE = ALL;
@@ -91,6 +93,7 @@ void setLow() // All outputs off (active low)
   digitalWrite(RELAY_4, HIGH);
   digitalWrite(RELAY_5, HIGH);
   digitalWrite(RELAY_6, HIGH);
+  digitalWrite(RELAY_7, HIGH);  // This relay output is currently unused
 
   Serial.println("All outpts OFF");
 }
@@ -112,14 +115,11 @@ void LED(int pin)
 {
   digitalWrite(LED0, LOW);
   digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
-  
-  if (pin == 0)
+
+  if (pin == 1)
     digitalWrite(LED0, HIGH);
-  else if (pin == 1)
-    digitalWrite(LED1, HIGH);
   else if (pin == 2)
-    digitalWrite(LED2, HIGH);
+    digitalWrite(LED1, HIGH);
 }
 
 void runCycle(int cc)
@@ -156,19 +156,19 @@ void stateInit(void)
   {
     case ALL: // This mode turns all outputs on
       setHigh();
-      LED(0);
+      LED(1);
     break;
 
     case CYCLE: // This mode cycles outputs
       setLow();
       cycleCount = 0;
-      LED(1);
+      LED(2);
     break;
 
     case MANUAL: // This mode allows manual selection of output
       cycleCount = 0;
       setLow();
-      LED(2);
+      LED(0);
     break;
 
     default:
